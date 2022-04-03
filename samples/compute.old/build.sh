25cfa5d2de499737053a39ac416449847ce91401
Philippe Charrière <ph.charriere@gmail.com>
2022-04-03 18:27:45 +0000

    📝 save

--- samples/compute.old/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#tinygo build -o main.wasm -target wasm ./main.go
-
-version="0.0.1"
-
-tinygo build -o compute_${version}.wasm -target wasi ./main.go
-
-ls -lh *.wasm
-
--- samples/compute.old/call-compute.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST -d '{"FirstName": "John", "LastName": "Doe"}' http://localhost:8087
